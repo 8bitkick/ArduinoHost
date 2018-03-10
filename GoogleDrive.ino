3df56602fa33758dda6e8a57c5c7f782223dbbf8
8bitkick <8bitkick@users.noreply.github.com>
2018-03-09 18:08:26 -0800

    Using ArduinoHttpClient

--- GoogleDrive.ino
+++ GoogleDrive.ino
@@ -1,11 +1,12 @@
 // 
-// OAuth 2.0 client to access Google APIs
-// Scope is access Google Drive files this app creates
+// OAuth 2.0 for Google APIs on Arduino
 //
+// To set-up app on cloud see prerequisites section:
 // https://developers.google.com/identity/protocols/OAuth2ForDevices
 //
-// Incomplete / not recommended for use!
+//
 
+#include <ArduinoHttpClient.h>
 #include <ArduinoJson.h>
 #include <SPI.h>
 #include <WiFi101.h>
@@ -14,32 +15,37 @@
 
 // WiFi
 char ssid[] = SECRET_SSID;   
-char pass[] = SECRET_PASS;   
+char pass[] = SECRET_PASS;  
+int status = WL_IDLE_STATUS;
+WiFiSSLClient wifi;
+HttpClient client = HttpClient(wifi, "accounts.google.com", 443);
+
+struct HttpResponse {
+ int statusCode;
+ String response;
+};
 
 
-// Google API 
+// Google API - Scope is acces to ussr Google Drive files this app creates
+String scope = "https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.file";
 String client_id = SECRET_CLIENT_ID;
 String client_secret = SECRET_CLIENT_SECRET;
+String access_token;
 
-  
-// Keep the tokens in flash when granted
+// Save refresh token in flash 
 // So we can skip user authorization after reset
 
 typedef struct {
   boolean valid;
-  char access[130];
-  char refresh[130];
-} Tokens;
+  // Google OAuth fresh token 
+  char refresh_token[70];
+} SavedState;
+
+FlashStorage(flash, SavedState);
+SavedState mySavedState;
 
 
-FlashStorage(flash, Tokens);
-Tokens googleapi_tokens;
-  
-int status = WL_IDLE_STATUS;
-
-WiFiClient client;
-
-// JSON ARDUINO 
+// JSON Arduino  
 const size_t bufferSize = JSON_OBJECT_SIZE(5) + 230;
 DynamicJsonBuffer jsonBuffer(bufferSize);
   
@@ -50,7 +56,14 @@ DynamicJsonBuffer jsonBuffer(bufferSize);
 
 
 void setup_wifi(){
-   // check for the presence of the shield:
+
+  IPAddress ip(172, 20, 20, 61); 
+  IPAddress subnet(255, 255, 255, 0); 
+  IPAddress dns(75, 75, 75, 75); 
+  IPAddress gateway(172, 20, 20, 1); 
+  WiFi.config(ip, dns, gateway, subnet); 
+  
+   // check for the presence of the shield
   if (WiFi.status() == WL_NO_SHIELD) {
     Serial.println("WiFi shield not present");
     // don't continue:
@@ -64,11 +77,14 @@ void setup_wifi(){
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
 
-    // wait 10 seconds for connection:
-    delay(10000);
+    // wait 1 seconds for connection:
+    delay(3000);
   }
   Serial.println("Connected to wifi");
+  
   printWifiStatus();
+  
+  
 }
 
 void printWifiStatus() {
@@ -94,24 +110,25 @@ void printWifiStatus() {
 // ==========================================================================
 
 void setup_auth(){
-  // Google APIs authentication
-  //--------------------------------------------
+
   // We're an app requesting access to Google Drive for only the files we create
   // scope = https://www.googleapis.com/auth/drive.file
   
-  Serial.println("Authentication required"); 
+  Serial.println("Google API: Authentication required"); 
   
-  http_post("accounts.google.com","/o/oauth2/device/code", "application/x-www-form-urlencoded", 
-  "client_id=" + client_id + "&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive.file");
+  String content = "client_id=" + client_id + "&scope=" + scope;
+  HttpResponse googRequestCodes = https_post("accounts.google.com","/o/oauth2/device/code", "application/x-www-form-urlencoded", content);
   
   // Parse response
-  JsonObject& parsedJSON = jsonBuffer.parseObject(client);
+  JsonObject& parsedJSON = jsonBuffer.parseObject(googRequestCodes.response);
 
-  const char* verification_url = parsedJSON["verification_url"]; 
+  String verification_url = parsedJSON["verification_url"]; 
   int expires_in = parsedJSON["expires_in"]; 
   int interval = parsedJSON["interval"]; 
-  const char* device_code = parsedJSON["device_code"]; 
-  const char* user_code = parsedJSON["user_code"]; 
+  String device_code = parsedJSON["device_code"]; 
+  String user_code = parsedJSON["user_code"]; 
+  
+  jsonBuffer.clear(); // Allocated memory is freed
   
   // Display user code and request user grants this app permission 
   Serial.println("\n\n*********************");
@@ -122,142 +139,106 @@ void setup_auth(){
   Serial.println("*********************");
 
   // Poll to see if they have granted permission
-  while (http_post("www.googleapis.com","/oauth2/v4/token", "application/x-www-form-urlencoded", 
+  HttpResponse googAccessRequest;
+  
+  while (googAccessRequest.statusCode != 200){
+  
+  googAccessRequest = https_post("www.googleapis.com","/oauth2/v4/token", "application/x-www-form-urlencoded", 
   "client_id=" + client_id + 
   "&client_secret=" + client_secret + 
   "&code=" + device_code + 
-  "&grant_type=http%3A%2F%2Foauth.net%2Fgrant_type%2Fdevice%2F1.0"))
-  {delay(5000);};
+  "&grant_type=http%3A%2F%2Foauth.net%2Fgrant_type%2Fdevice%2F1.0");
+  
+  delay(5000);
+  }
   
    // Parse response
-  JsonObject& parsedJSON2 = jsonBuffer.parseObject(client);
-
-  String access_token = parsedJSON2["access_token"]; 
-  int token_expires_in = parsedJSON2["expires_in"]; 
-  String refresh_token = parsedJSON2["refresh_token"]; 
+  JsonObject& parsedJSON2 = jsonBuffer.parseObject(googAccessRequest.response);
+  
+  access_token = parsedJSON2["access_token"].as<String>(); 
+  //int token_expires_in = parsedJSON2["expires_in"]; 
+  String refresh_token = parsedJSON2["refresh_token"];     // To be stored in Flash
+  
+  jsonBuffer.clear(); // Allocated memory is freed
   
   Serial.println("\nAccess granted");
   Serial.println("*********************");
-  
-   // We now have Google API access to files we create on users Google Drive
-  Serial.println(access_token);
-  
-  http_get("www.googleapis.com","/drive/v3/files",access_token);
-  
-  // Write tokens to flash memory
-  access_token.toCharArray(googleapi_tokens.access, 130);
-  refresh_token.toCharArray(googleapi_tokens.refresh, 130);
 
-  googleapi_tokens.valid = true;
-
-  flash.write(googleapi_tokens);
-
+  refresh_token.toCharArray(mySavedState.refresh_token, 70);
+  mySavedState.valid = true;
+  
 }
 
-/* TODO
-void token_refresh(const char* refresh_token){ 
+
+void token_refresh(){ 
   // We're requesting a token refresh
-  http_post("www.googleapis.com","/oauth2/v4/token", "application/x-www-form-urlencoded", 
+  HttpResponse googTokenRefresh = https_post("www.googleapis.com","/oauth2/v4/token", "application/x-www-form-urlencoded", 
   "client_id=" + client_id + 
   "&client_secret=" + client_secret + 
-  "&code=" + refresh_token + 
-  "&refresh_token=" + device_code + 
+  "&refresh_token=" + mySavedState.refresh_token + 
   "&grant_type=refresh_token");  
-    
+
    // Parse response
-  JsonObject& parsedJSON3 = jsonBuffer.parseObject(client);
-  const char* access_token = parsedJSON3["access_token"]; 
-  int token_expires_in = parsedJSON3["expires_in"]; 
+  JsonObject& parsedJSON3 = jsonBuffer.parseObject(googTokenRefresh.response);
+  access_token = parsedJSON3["access_token"].as<String>(); 
+  // int token_expires_in = parsedJSON3["expires_in"]; TODO
   
-  // Write tokens to flash memory
-  access_token.toCharArray(googleapi_tokens.access, 130);
-  refresh_token.toCharArray(googleapi_tokens.refresh, 130);
-  googleapi_tokens.valid = true;
-  flash.write(googleapi_tokens);
+  jsonBuffer.clear(); // Allocated memory is freed
   
-  Serial.println("Token refreshed");
-}*/
+  Serial.println("Google API: access token refreshed");
+}
 
 
 // POST
-int http_post(String host, String path, String contentType, String data) {
-  // NB host = server in this example
+HttpResponse https_post(String host, String path, String contentType, String data) {
+    Serial.println("POST: "+data);
   client.stop();
-  // if there's a successful connection:
-  if (client.connectSSL(host.c_str(), 443)) {
-    client.println("POST " + path + " HTTP/1.1");
-    client.println("Host: "+ host);
-    //client.println("Connection: close");
-    client.println("Content-Type: "+contentType);
-    client.print("Content-Length: ");
-    client.println(data.length(),DEC);
-    client.println("");
-    client.println(data);
-    
-  // Check response 
-  char status[32] = {0};
-  client.readBytesUntil('\r', status, sizeof(status));
-  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
-    //Serial.print(F("Unexpected response: "));
-    //Serial.println(status);
-    return 1;
-  }
+  client = HttpClient(wifi, host, 443);
 
-  char endOfHeaders[] = "\r\n\r\n";
-  if (!client.find(endOfHeaders)) {
-    Serial.println(F("Invalid response"));
-    return 1;
-  }
+  client.beginRequest();
+  client.post(path);
+  client.sendHeader("Content-Type", contentType);
+  client.sendHeader("Content-Length", data.length());
+  client.endRequest();
+  client.print(data);
 
-  client.readBytesUntil('\n', status, sizeof(status));  
-  }
-  else {
-    // if you couldn't make a connection:
-    Serial.println("connection failed");
-    return 1;
-  }
-  return 0;
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+
+  #ifdef DEBUG
+  Serial.print("Status code is: ");
+  Serial.println(statusCode);
+
+  Serial.print("Response is: ");
+  Serial.println(response);
+  #endif
+  
+  return HttpResponse {statusCode, response};
 }
 
 // GET
-int http_get(String host, String path, String auth) {
-  client.stop();
+HttpResponse https_get(String host, String path) {
+  
+  client.stop(); 
+  client = HttpClient(wifi, host, 443);
+  client.beginRequest();
+  client.get(path);
+  client.sendHeader("Authorization", "Bearer "+ access_token);
+  client.endRequest();
+    
+  int statusCode = client.responseStatusCode();
+  String response = client.responseBody();
+  
+  #ifdef DEBUG
+  Serial.print("Status code is: ");
+  Serial.println(statusCode);
 
-  // if there's a successful connection:
-  if (client.connectSSL(host.c_str(), 443)) {
-    client.println("GET " + path + " HTTP/1.1");
-    client.println("Host: "+ host);
-    client.println("Connection: close");
-    client.print  ("Authorization: Bearer ");
-    client.println(auth);
-    client.println();
-    
-  // Check response 
-  char status[32] = {0};
-  client.readBytesUntil('\r', status, sizeof(status));
-  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
-    //Serial.print(F("Unexpected response: "));
-    //Serial.println(status);
-    return 1;
-  }
-
-  char endOfHeaders[] = "\r\n\r\n";
-  if (!client.find(endOfHeaders)) {
-    Serial.println(F("Invalid response"));
-    return 1;
-  }
-    
-    
-  }
-  else {
-    // if you couldn't make a connection:
-    Serial.println("connection failed: ");
-    return 1;
-  }
-  return 0;
+  Serial.print("Response is: ");
+  Serial.println(response);
+  #endif
+  
+  return HttpResponse {statusCode, response};
 }
-
-
 
 
 // ==========================================================================
@@ -275,40 +256,50 @@ void setup() {
     ; // wait for serial port to connect. Needed for native USB port only
   }
 
+  // Restore state from flash
+  // ------------------------
+  mySavedState = flash.read();
+
+
   // WiFi set-up
-  //--------------------------------------------
+  
   setup_wifi();
   
-  // Google API access
-  // -------------------------------------------
-  
-  // Read tokens from flash 
-  googleapi_tokens = flash.read();
-  String access_token =  googleapi_tokens.access;
+  // Google API access - refresh 
 
-  if (googleapi_tokens.valid == true){
-    Serial.println("Tokens exist in flash, great"); 
-    // Check access token still works
-     if (http_get("www.googleapis.com","/drive/v3/files", access_token)) {
-        Serial.println("Tokens needs refreshing TODO"); // TODO track UTC + expiry 
-       // token_refresh(refresh_token);
-     } return; // TODO check for success here
-  } else setup_auth();
+  if (mySavedState.valid == true){
+    Serial.println("Google API: Refresh token exists in flash"); 
+    token_refresh(); // TODO track expiry time
+  } else {
+
+
+  // Google API access - new
+ 
+  setup_auth();
   
+  // Save refresh token to flash
+
+  flash.write(mySavedState);
+  
+  }
+  
+  // Test out an API call
+  
+  https_get("www.googleapis.com","/drive/v3/files");
+
 }
 
+
+
+  
 // ==========================================================================
 // SECTION: Loop
 // ==========================================================================
 
 
 void loop() {
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    Serial.write(c);
-  }
+
+
 }
 
 
